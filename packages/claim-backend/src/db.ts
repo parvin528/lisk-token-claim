d914bd5a4aa7430c3fa61ef3eab940507c6de243
Franco NG <franco.ng@lightcurve.io>
2024-05-08 16:18:43 +0200

    Used Pasword Path instead of hard coding password (#35)
    * Used Pasword Path instead of hard coding password
    
    * Update yaml and README according to PR Review suggestion
    
    ---------
    
    Co-authored-by: Sameer <sameer.subudhi@lightcurve.io>

--- packages/claim-backend/src/db.ts
+++ packages/claim-backend/src/db.ts
@@ -1,3 +1,5 @@
+import * as fs from 'fs';
+
 import { Sequelize } from 'sequelize-typescript';
 import Signature from './models/signature.model';
 
@@ -12,7 +14,7 @@ export class DB {
 			host: process.env.DB_HOST || '127.0.0.1',
 			database: process.env.DB_DATABASE || 'claim-backend',
 			username: process.env.DB_USERNAME || 'claim-backend',
-			password: process.env.DB_PASSWORD || 'passwd',
+			password: fs.readFileSync(process.env.DB_PASSWORD_PATH || 'db_password.txt', 'utf-8'),
 			models: [__dirname + '/models/*.model.ts'],
 			port: Number(process.env.DB_PORT) || 5432,
 			logging: process.env.DB_LOGGING !== 'false',
